b8d1f784cc7ff3e844b6a097bf8925c3b3757c8e
brenoakese <amorimbreno1207@gmail.com>
2025-02-23 18:51:58 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const ProductPage = () => {
+    return (
+    <div>
+      
+        <h1 className="text-red-500">Product Page</h1>;
+        <Button>Click Me</Button>
+        <Input placeholder="Enter your name" />
+    </div>
+    );
+}
+ 
+export default ProductPage;
